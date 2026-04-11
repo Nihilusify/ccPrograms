4bce38c815f3a9bd2faad39320d920274691e3bd
Terence van Jaarsveldt <vanjaarsveldttl@gmail.com>
2026-04-11 11:37:08 +0200

    Initial new manager app shell

---
+++ ccManager2/src/lib/server/db/schema.ts
@@ -0,0 +1,7 @@
+import { pgTable, serial, integer, text } from 'drizzle-orm/pg-core';
+
+export const task = pgTable('task', {
+	id: serial('id').primaryKey(),
+	title: text('title').notNull(),
+	priority: integer('priority').notNull().default(1)
+});
